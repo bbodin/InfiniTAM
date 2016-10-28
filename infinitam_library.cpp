3cf808fcda91f4531b8b392a3e21b2883328612b
Bruno Bodin (zebulon) <bbodin@inf.ed.ac.uk>
2016-10-28 17:27:01 +0100

    more changes

--- infinitam_library.cpp
+++ infinitam_library.cpp
@@ -17,27 +17,92 @@
 
 static ITMMainEngine* mainEngine ;
 
+static ITAMConfiguration * config;
+static Vector2i inputSize;
+
 static  sb_uchar3*   inputRGB;
 static  uint16_t*    inputDepth;
-static ITAMConfiguration * config;
-static Vector2i img_size;
+
 static ITMUChar4Image* rgbwrapper ;
 static ITMShortImage * depthwrapper ;
-
 static ITMUChar4Image *rgb      ;
 static ITMUChar4Image *depth    ;
 static ITMUChar4Image *raycast  ;
+
 static ITMLibSettings *internalSettings;
 
 static ITMRGBDCalib calibration;
+
+
+/**
+ * Initialization.
+ */
 
 void sb_new_slam_configuration(SLAMBenchConfiguration ** slam_settings) {
     *slam_settings = new ITAMConfiguration();
 }
 
+
 bool sb_init_slam_system(SLAMBenchConfiguration * slam_settings)  {
 
     config = dynamic_cast<ITAMConfiguration*> (slam_settings);
+
+
+    /**
+     * Retrieve RGB and Depth sensors,
+     *  - check input_size are the same
+     *  - check camera are the same
+     *  - get input_file
+     */
+
+    RGBFrame* rgb_frame = NULL;
+    DepthFrame* d_frame = NULL;
+
+    for (Sensor *s : slam_settings->get_sensors()) {
+        if (s->get_format() == RGB_FRAME) {
+            rgb_frame = dynamic_cast<RGBFrame*>(s);
+        }
+        if (s->get_format() == DEPTH_FRAME) {
+            d_frame = dynamic_cast<DepthFrame*>(s);
+        }
+    }
+
+    if (not (rgb_frame and d_frame)) {
+        std::cerr << "Invalid sensors found, RGB or Depth not found." << std::endl;
+        return false;
+    }
+
+    if  (rgb_frame->getSize() != d_frame->getSize()) {
+        std::cerr << "Invalid sensors found, different sizes" << std::endl;
+        return false;
+    }
+
+
+
+
+    sb_float4 depth_camera =  make_sb_float4(
+                d_frame->getIntrinsics()[0],
+                d_frame->getIntrinsics()[1],
+                d_frame->getIntrinsics()[2],
+                d_frame->getIntrinsics()[3]);
+
+     depth_camera.x = depth_camera.x * d_frame->getSize()[0];
+     depth_camera.y = depth_camera.y * d_frame->getSize()[1];
+     depth_camera.z = depth_camera.z * d_frame->getSize()[0];
+     depth_camera.w = depth_camera.w * d_frame->getSize()[1];
+
+
+     sb_float4 rgb_camera =  make_sb_float4(
+                rgb_frame->getIntrinsics()[0],
+                rgb_frame->getIntrinsics()[1],
+                rgb_frame->getIntrinsics()[2],
+                rgb_frame->getIntrinsics()[3]);
+
+     rgb_camera.x = rgb_camera.x * rgb_frame->getSize()[0];
+     rgb_camera.y = rgb_camera.y * rgb_frame->getSize()[1];
+     rgb_camera.z = rgb_camera.z * rgb_frame->getSize()[0];
+     rgb_camera.w = rgb_camera.w * rgb_frame->getSize()[1];
+
 
     // Teddy RGB camera settings :
     // 0.787907813,1.049802083,0.550714063,0.5670875
@@ -45,99 +110,119 @@ bool sb_init_slam_system(SLAMBenchConfiguration * slam_settings)  {
     // Teddy D camera settings :
     // 0.896421875,1.196654167,0.541360938,0.518814583
 
-    sb_float4 camera =  make_sb_float4(
-            config->get_camera().x,
-            config->get_camera().y,
-            config->get_camera().z,
-            config->get_camera().w);
-
-    camera.x = camera.x *  config->get_input_size().x;
-    camera.y = camera.y *  config->get_input_size().y;
-    camera.z = camera.z *  config->get_input_size().x;
-    camera.w = camera.w *  config->get_input_size().y;
 
     internalSettings = new ITMLibSettings();
 
-    img_size = {(int)config->get_input_size().x, (int)config->get_input_size().y};
+    inputSize[0] = rgb_frame->getSize()[0] ;
+    inputSize[1] = rgb_frame->getSize()[1] ;
 
 
 
-    calibration.intrinsics_rgb.SetFrom(camera.x, camera.y,camera.z, camera.w, config->get_input_size().x, config->get_input_size().y);
-    calibration.intrinsics_d.SetFrom(camera.x, camera.y,camera.z, camera.w, config->get_input_size().x, config->get_input_size().y);
-    calibration.intrinsics_d.SetFrom( 573.71,574.394,346.471,249.031,640,480);
+    calibration.intrinsics_rgb.SetFrom(rgb_camera.x, rgb_camera.y,rgb_camera.z, rgb_camera.w, rgb_frame->getSize()[0], rgb_frame->getSize()[1]);
+    calibration.intrinsics_d.SetFrom(depth_camera.x, depth_camera.y,depth_camera.z, depth_camera.w, d_frame->getSize()[0], d_frame->getSize()[1]);
+
+    //calibration.intrinsics_d.SetFrom( 573.71,574.394,346.471,249.031,640,480);
 
 
     Matrix4f calib;
-    calib.m00 = 1.0f; calib.m10 = 0.0f; calib.m20 = 0.0f;   calib.m30 = 0.0f;
-    calib.m01 = 0.0f; calib.m11 = 1.0f; calib.m21 = 0.0f;   calib.m31 = 0.0f;
-    calib.m02 = 0.0f; calib.m12 = 0.0f; calib.m22 = 1.0f;   calib.m32 = 0.0f;
-    calib.m03 = 0.0f; calib.m13 = 0.0f; calib.m23 = 0.0f;   calib.m33 = 1.0f;
+
+    // ICLNUIM
+    //calib.m00 = 1.0f; calib.m10 = 0.0f; calib.m20 = 0.0f;   calib.m30 = 0.0f;
+    //calib.m01 = 0.0f; calib.m11 = 1.0f; calib.m21 = 0.0f;   calib.m31 = 0.0f;
+    //calib.m02 = 0.0f; calib.m12 = 0.0f; calib.m22 = 1.0f;   calib.m32 = 0.0f;
+    //calib.m03 = 0.0f; calib.m13 = 0.0f; calib.m23 = 0.0f;   calib.m33 = 1.0f;
 
 
     // Teddy D trafo_rgb_to_depth calib :
-    calib.m00 =  0.9997490f; calib.m10 = 0.00518867f; calib.m20 =  0.0217975f;   calib.m30 =  0.2243070f;
-    calib.m01 = -0.0051649f; calib.m11 = 0.99998600f; calib.m21 = -0.0011465f;   calib.m31 = -0.5001670f;
-    calib.m02 = -0.0218031f; calib.m12 = 0.00103363f; calib.m22 =  0.9997620f;   calib.m32 =  0.0151706f;
-    calib.m03 =  0.0000000f; calib.m13 = 0.00000000f; calib.m23 =  0.0000000f;   calib.m33 =  1.0000000f;
+    //calib.m00 =  0.9997490f; calib.m10 = 0.00518867f; calib.m20 =  0.0217975f;   calib.m30 =  0.2243070f;
+    //calib.m01 = -0.0051649f; calib.m11 = 0.99998600f; calib.m21 = -0.0011465f;   calib.m31 = -0.5001670f;
+    //calib.m02 = -0.0218031f; calib.m12 = 0.00103363f; calib.m22 =  0.9997620f;   calib.m32 =  0.0151706f;
+    //calib.m03 =  0.0000000f; calib.m13 = 0.00000000f; calib.m23 =  0.0000000f;   calib.m33 =  1.0000000f;
+
+
+    // Ensure RGB is identity to set depth as the trnaslation
+    // TODO : Could do the matrix transformation but well,  not now
+
+    if  (rgb_frame->getDirection() !=  Eigen::Matrix4f::Identity()) {
+        std::cerr << "Invalid direction for rgb" << std::endl;
+        return false;
+    }
+
+    // Teddy D trafo_rgb_to_depth calib :
+    calib.m00 = d_frame->getDirection()(0,0); calib.m10 = d_frame->getDirection()(1,0); calib.m20 = d_frame->getDirection()(2,0); calib.m30 = d_frame->getDirection()(3,0);
+    calib.m01 = d_frame->getDirection()(0,1); calib.m11 = d_frame->getDirection()(1,1); calib.m21 = d_frame->getDirection()(2,1); calib.m31 = d_frame->getDirection()(3,1);
+    calib.m02 = d_frame->getDirection()(0,2); calib.m12 = d_frame->getDirection()(1,2); calib.m22 = d_frame->getDirection()(2,2); calib.m32 = d_frame->getDirection()(3,2);
+    calib.m03 = d_frame->getDirection()(0,3); calib.m13 = d_frame->getDirection()(1,3); calib.m23 = d_frame->getDirection()(2,3); calib.m33 = d_frame->getDirection()(3,3);
+
+
 
     calibration.trafo_rgb_to_depth.SetFrom(calib);
 
 
 
     //std::stringstream src("affine 0.0002 0.0");
-    std::stringstream src("kinect 1135.09 0.0819141");
+    //std::stringstream src("kinect 1135.09 0.0819141");
 
-    std::string word;
-    float a,b;
-    src >> word >> a >> b;
-    if (src.fail()) return false;
     ITMDisparityCalib::TrafoType type = ITMDisparityCalib::TRAFO_KINECT;
-
-
-    if (word.compare("kinect") == 0) {
-        type = ITMDisparityCalib::TRAFO_KINECT;
-        src >> a;
-    } else if (word.compare("affine") == 0) {
-        type = ITMDisparityCalib::TRAFO_AFFINE;
-    } else {
-        std::stringstream wordstream(word);
+    switch (d_frame->getDisparityType()) {
+        case DISPARITY_KINECT:
+            type = ITMDisparityCalib::TRAFO_KINECT;
+            break;
+        case DISPARITY_AFFINE:
+            type = ITMDisparityCalib::TRAFO_AFFINE;
+            break;
+        default :
+            exit(1);
     }
 
-
-    if ((a == 0.0f) && (b == 0.0f)) {
-        type = ITMDisparityCalib::TRAFO_AFFINE;
-        a = 1.0f/1000.0f; b = 0.0f;
-    }
-    calibration.disparityCalib.SetFrom(a, b, type);
+    calibration.disparityCalib.SetFrom(d_frame->getDisparityParams()[0], d_frame->getDisparityParams()[1], type);
 
 
-    rgbwrapper    = new ITMUChar4Image (img_size,true, false) ;
-    depthwrapper  = new ITMShortImage  (img_size,true, false) ;
+    rgbwrapper    = new ITMUChar4Image ( inputSize,true, false) ;
+    depthwrapper  = new ITMShortImage  ( inputSize,true, false) ;
 
-    mainEngine = new ITMMainEngine( internalSettings, &calibration, img_size, img_size);
+    mainEngine = new ITMMainEngine( internalSettings, &calibration, inputSize, inputSize);
 
-    inputDepth = (uint16_t*) malloc(sizeof(uint16_t) * config->get_input_size().x* config->get_input_size().y);
-    inputRGB = (sb_uchar3*) malloc(sizeof(sb_uchar3) * config->get_input_size().x* config->get_input_size().y);
+    inputDepth = (uint16_t*) malloc(sizeof(uint16_t) *  inputSize[0] * inputSize[1]);
+    inputRGB = (sb_uchar3*) malloc(sizeof(sb_uchar3) *  inputSize[0] * inputSize[1]);
 
 
 
 
-        return true;
+    return true;
 
 }
 
-bool sb_update_frame (void * data, frame_format type) {
-    switch (type) {
-    case DEPTH_FRAME : memcpy ( inputDepth,  data , img_size.x * img_size.y * sizeof (uint16_t)); return true;
-    case RGB_FRAME   : memcpy ( inputRGB  ,  data , img_size.x * img_size.y * sizeof (sb_uchar3)); return true;
+bool sb_clean_slam_system() {
+
+    delete mainEngine;
+    delete internalSettings;
+
+    if (raycast) delete raycast;
+    if (depth) delete depth;
+    if (rgb) delete rgb;
+    if (inputRGB) delete inputRGB;
+
+    return true;
+}
+
+/*
+ * Process frames.
+ */
+
+bool sb_update_frame (void * data, Sensor* s)  {
+    switch (s->get_format()) {
+    case DEPTH_FRAME : memcpy ( inputDepth,  data , inputSize.x * inputSize.y * sizeof (uint16_t)); return true;
+    case RGB_FRAME   : memcpy ( inputRGB  ,  data , inputSize.x * inputSize.y * sizeof (sb_uchar3)); return true;
        default : return false;
-       };
+    };
 }
+
 bool sb_process_once ()  {
 
     //if (imuSource != NULL) mainEngine->ProcessFrame(inputRGBImage, inputRawDepthImage, inputIMUMeasurement);
 
-        for (int i = 0 ; i < img_size.x * img_size.y ; i++) {
+        for (int i = 0 ; i < inputSize[0] * inputSize[1] ; i++) {
             rgbwrapper->GetData(MEMORYDEVICE_CPU)[i] = {inputRGB[i].x,inputRGB[i].y,inputRGB[i].z,255};
             depthwrapper->GetData(MEMORYDEVICE_CPU)[i] = inputDepth[i];
 
@@ -161,19 +246,30 @@ bool sb_process_once ()  {
 
 }
 
+
+
+
+
+/*
+ * Output part.
+ *
+ */
+
+
 sb_float3 sb_get_position () {
 
     ITMTrackingState *state = mainEngine->GetTrackingState();
-
-	sb_float3 res = {state->pose_d->GetT()[0],state->pose_d->GetT()[1],state->pose_d->GetT()[2]};
+    sb_float3 res = {state->pose_d->GetT()[0],state->pose_d->GetT()[1],state->pose_d->GetT()[2]};
     return  res;
 }
 
 Eigen::Matrix4f  sb_get_pose ()  {
-    assert(false);
+    // TODO : This getter has not been checked yet
     Eigen::Matrix4f res;
+    mainEngine->GetTrackingState()->pose_d->GetM().getValues(&res(0,0));
     return res;
 }
+
 bool             sb_get_tracked  ()  {
     return true;
 }
@@ -186,33 +282,26 @@ bool sb_save_map (const char * filename , map_format format) {
 
 }
 
-bool sb_clean_slam_system() {
 
-    delete mainEngine;
-    delete internalSettings;
-
-    if (raycast) delete raycast;
-    if (depth) delete depth;
-    if (rgb) delete rgb;
-    if (inputRGB) delete inputRGB;
-
-    return true;
-}
+/*
+ * Rendering part
+ */
 
 
 bool sb_initialize_ui(SLAMBenchUI * ui ) {
 
-    rgb = new ITMUChar4Image(img_size, true, false);
-    depth = new ITMUChar4Image(img_size, true, false);
-    raycast = new ITMUChar4Image(img_size, true, false);
+    rgb = new ITMUChar4Image(inputSize, true, false);
+    depth = new ITMUChar4Image(inputSize, true, false);
+    raycast = new ITMUChar4Image(inputSize, true, false);
 
 
-    ui->init(0, RGB24, img_size.x,img_size.y);
-    ui->init(1, RGBA32,  img_size.x,img_size.y);
-    ui->init(2, RGBA32,  img_size.x,img_size.y);
-    ui->init(3, RGBA32,  img_size.x,img_size.y);
+    ui->init(0, RGB24, inputSize[0], inputSize[1]);
+    ui->init(1, RGBA32, inputSize[0],inputSize[1]);
+    ui->init(2, RGBA32, inputSize[0],inputSize[1]);
+    ui->init(3, RGBA32, inputSize[0],inputSize[1]);
     return true;
 }
+
 
 bool sb_update_ui(SLAMBenchUI * ui) {
 
